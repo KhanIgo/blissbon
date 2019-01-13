b17bebf08dde7a44ea9bb2f92cfe8fa8852b6871
Igor' Khan <igor.swit4@gmail.com>
2019-01-13 10:58:47 +1000

    build added

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,5 +2,14 @@
 jQuery(function($){
     let $body = $('body');
  
+    $body.on('click', '.js-close-modal', function(){
+        $(this).closest('.ui-modal').removeClass('active');
+    });
     
+    $body.on('click', '.js-show-modal', function(e){
+        e.preventDefault();
+        let id = '#' + $(this).data('modal');
+        let $modal = $(id);
+        if($modal.length) $modal.addClass('active');
+    });
 });
